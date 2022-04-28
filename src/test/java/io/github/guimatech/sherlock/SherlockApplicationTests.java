62d214ae4770f9558f22ecdee39f53ac1aff6068
guimatech <guimaraes-lucas@outlook.com.br>
2022-04-28 15:46:05 -0300

    feat: add initial files

---
+++ src/test/java/io/github/guimatech/sherlock/SherlockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guimatech.sherlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SherlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
